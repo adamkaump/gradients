e24f4aa8b898ed262940a560e26144aacd7d8e52
Adam Kaump <adam.kaump@gmail.com>
2015-02-20 23:53:16 -0500

    Initial Commit

---
+++ Gradient Background Generator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Gradient Background Generator
+//
+//  Created by Adam Kaump on 2/20/15.
+//  Copyright (c) 2015 Adam Kaump. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
